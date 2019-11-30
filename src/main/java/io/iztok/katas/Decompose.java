eda537ea6fd4076f9b8da38fe6b2ec2432317047
Iztok Svetik <iztok.svetik@posteo.de>
2019-11-30 20:38:03 +0100

    Decompose

--- src/main/java/io/iztok/katas/Decompose.java
+++ src/main/java/io/iztok/katas/Decompose.java
@@ -1,37 +1,47 @@
 package io.iztok.katas;
-import java.util.Comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 class Decompose {
     public Decompose() {
     }
 
     public String decompose(long n) {
-        Stream<Long> possibleSizes = LongStream.range(1, 1 << (n-1))
-                .boxed().sorted(Comparator.reverseOrder());
-
-        List<Long> bestCombination = permutations(possibleSizes, n - 1)
-                .filter(permutation -> permutation.stream()
-                        .reduce(0L, (a, b) -> a + (b*b)) == n * n)
-                .findFirst()
-                .orElse(null);
-
-        if (bestCombination == null) {
+        var result = find(n, n*n, n*n, new ArrayList<>());
+        if (result == null) {
             return null;
         }
-        return bestCombination.stream()
-                .filter(number -> number != 0)
-                .map(Object::toString)
-                .collect(Collectors.joining(" "));
-
+        Collections.reverse(result);
+        return result.stream().map(Object::toString).collect(Collectors.joining(" "));
     }
 
-    private Stream<List<Long>> permutations(Stream<Long> numbers, long length) {
-        return numbers.map(number -> LongStream.range(1, length + 1)
-                        .map(pos -> ((1 << pos - 1) & number) != 0 ? pos : 0)
-                        .boxed().collect(Collectors.toList()));
+    private Long next(Long rest) {
+        return (long) Math.sqrt(rest);
+    }
+
+    private List<Long> find(Long start, Long rest, Long total, List<Long> current) {
+        if (rest == 0) {
+            if (current.stream().map(x -> x*x).reduce(Long::sum).orElse(0L).equals(total)) {
+                return current;
+            }
+            return null;
+        }
+        var next = start - 1;
+        if (next*next > rest) {
+            next = next(rest);
+        }
+        while (next > 0) {
+            var newCurrent = new ArrayList<>(current);
+            newCurrent.add(next);
+            var newResult = find(next, (rest-next*next), total, newCurrent);
+            if (newResult != null) {
+                return newResult;
+            }
+            next--;
+        }
+        return null;
     }
 }
--- src/test/java/io/iztok/katas/DecomposeTest.java
+++ src/test/java/io/iztok/katas/DecomposeTest.java
@@ -1,12 +1,51 @@
 package io.iztok.katas;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecomposeTest {
+    // 50 - [1,3,5,8,49]
+    // 625 - [2,5,8,34,624]
+    // 11 - [1,2,4,10]
+    // 12 - [1,2,3,7,9]
+    // 44 - [2,3,5,7,43]
+    private Decompose decompose;
+    @BeforeEach
+    void setUp() {
+        decompose = new Decompose();
+    }
+
     @Test
-    public void shouldDecomopse() {
+    void test11() {
+        assertEquals("1 2 4 10", decompose.decompose(11));
+    }
+
+    @Test
+    void test12() {
+        assertEquals("1 2 3 7 9", decompose.decompose(12));
+    }
+
+    @Test
+    void test50() {
+        assertEquals("1 3 5 8 49", decompose.decompose(50));
+    }
+
+    @Test
+    void test625() {
+        assertEquals("2 5 8 34 624", decompose.decompose(625));
+    }
+
+    @Test
+    void test44() {
+        assertEquals("2 3 5 7 43", decompose.decompose(44));
+    }
+
+    @Test
+    void test4() {
+        assertNull(decompose.decompose(4));
     }
 
 }
